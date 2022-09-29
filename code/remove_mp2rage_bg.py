96b3bd36e77ca46262d4827b1b5c452987cd0556
Alfredo Lucas <allucas@vagelos-ve703-14706.apn.wlan.private.upenn.edu>
2022-09-29 10:15:30 -0400

    added function to filter mp2rage background noise

---
+++ code/remove_mp2rage_bg.py
@@ -0,0 +1,47 @@
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import medfilt
+
+## Argument Parser
+import argparse
+import os
+import subprocess
+
+# Parse all the required arguments
+parser = argparse.ArgumentParser()
+
+# Global arguments
+parser.add_argument("-inv1", "--inv1_path")
+parser.add_argument("-inv2","--inv2_path")
+parser.add_argument("-uni","--uni_path")
+parser.add_argument("-o","output_nifti")
+
+
+
+args = parser.parse_args()
+
+inv1_name = parser.inv1_path
+inv2_name = parser.inv2_path
+uni_name = parser.uni_path
+output = parser.output_nifti
+
+inv1 = nib.load(inv1_name).get_fdata()
+inv2 = nib.load(inv2_name).get_fdata()
+uni = nib.load(uni_name).get_fdata()
+
+
+beta=(np.mean(uni[-30:-10,-30:-10,-30:-10])*200)
+INV1final = inv1
+uni_fixed = np.real(((INV1final)*inv2-beta) / (np.abs(INV1final)**2 + np.abs(inv2)**2 + 2*beta))
+
+uni_fixed_mask = -uni_fixed
+
+uni_fixed_mask[uni_fixed>0] = -uni_fixed[uni_fixed>0]*(-1)
+
+uni_fixed_mask = medfilt(uni_fixed_mask, 5)
+
+uni_fixed_new = uni*(uni_fixed_mask<0.45)
+
+uni_fixed_nifti = nib.Nifti1Image(uni_fixed_new, nib.load(uni_name).affine)
+nib.save(uni_fixed_nifti, output)
